f417847319629d0f552fb9e2978a488fa35eb614
Ryan Maloney <rypmaloney@gmail.com>
2021-06-11 11:05:12 -0400

    Created basic document structure in HTML/CSS

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,9 @@
-console.log("if you see me, everything is A O K");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('if you see me, everything is A O K')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jbGVhbi10by1kby8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBLGlEIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaWYgeW91IHNlZSBtZSwgZXZlcnl0aGluZyBpcyBBIE8gSycpIl0sInNvdXJjZVJvb3QiOiIifQ==
