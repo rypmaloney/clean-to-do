9a8cf07f5434997644700aeb310d433ac9be052c
Ryan Maloney <rypmaloney@gmail.com>
2021-06-13 12:55:09 -0400

    created project array, function to add project to array, and function to add list items to individual projects

--- src/index.js
+++ src/index.js
@@ -27,6 +27,99 @@ todayList = [
     }
 ]
 
+
+let projects = [
+    {
+        name: "Today",
+        description: 'Tasks that need to be completed today',
+        complete: false,
+        list: [{
+            name: 'This is my first to do',
+            priority: 'high',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my second to do',
+            priority: 'medium',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my third thing to do',
+            priority: 'low',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my fourth thing to do',
+            priority: 'high',
+            complete: false, 
+            
+        }]
+        
+    },
+    {
+        name: "This week",
+        description: 'Tasks that need to be completed this week',
+        complete: false,
+        list: [{
+            name: 'This is my first to do',
+            priority: 'high',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my second to do',
+            priority: 'medium',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my third thing to do',
+            priority: 'low',
+            complete: false, 
+            
+        },
+        {
+            name: 'This is my fourth thing to do',
+            priority: 'high',
+            complete: false, 
+            
+        }]
+        
+    },
+]
+
+
+
+
+
+
+let currentList = projects[0]
+
+
+const createProject = (name, description) => {
+    projects.push(
+        {
+            name,
+            list: [],
+            description,
+            complete: false,
+        }
+    )
+    {return name, description}
+}
+
+function addNewDo(currentProject, name, priority){
+    currentProject.list.push({
+        name: name,
+        priority: priority,
+        complete: false,
+    })
+}
+
+
 function displayList(listArray) {
     let list = document.getElementById('current-list');
         for (i = 0; i < listArray.length; i++) {
