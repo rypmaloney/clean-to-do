abf48f9a802817940b9aba0b249550bcf2e80270
Ryan Maloney <rypmaloney@gmail.com>
2021-06-15 09:49:31 -0400

    New project form popup added

--- src/index.js
+++ src/index.js
@@ -150,13 +150,18 @@ function addItemTransition(){
 
 
 let add = document.getElementById('add');
-add.addEventListener('click',()=>toggleForm())
+add.addEventListener('click',()=>toggleItemForm())
 
 let submitNewItemBtn = document.getElementById('submitNewItemBtn');
-submitNewItemBtn.addEventListener('click', () => toggleForm());
+submitNewItemBtn.addEventListener('click', () => toggleItemForm());
 
+let addProj = document.getElementById('addProj');
+addProj.addEventListener('click', () => toggleProjForm());
 
-function toggleForm(){
+let submitNewProjBtn = document.getElementById('submitNewProjBtn');
+submitNewProjBtn.addEventListener('click', () => toggleProjForm());
+
+function toggleItemForm(){
     let newItemSection = document.getElementById('new-item');
     let add = document.getElementById('add');
     let form = document.getElementById('newItemForm');
@@ -165,5 +170,13 @@ function toggleForm(){
     form.classList.toggle('hide');
 }
 
+function toggleProjForm(){
+    let newProjSection = document.getElementById('projectForm');
+    let add = document.getElementById('addProj');
+
+    newProjSection.classList.toggle('form-hide');
+
+}
 
 displayList(todayList)
+
